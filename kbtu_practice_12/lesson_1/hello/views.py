ed331cb1f926ac4132d6043e068bb5d4a78285cc
Zhannel Omarova <98833988+zhonniks@users.noreply.github.com>
2024-02-03 12:35:13 +0600

    added 3 views

--- kbtu_practice_12/lesson_1/hello/views.py
+++ kbtu_practice_12/lesson_1/hello/views.py
@@ -1,8 +1,19 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
 
-def index(request):
-    return HttpResponse('LOL KEk ')
 
-def other(request):
-    return HttpResponse('Today Django Day')
+def main_view(request):
+    s = "This is  main view"
+    return HttpResponse(s)
+
+
+def basic_view(request):
+    s = "This is basic view"
+    return HttpResponse(s)
+
+
+def test_view(request):
+    s = "Test View"
+    return HttpResponse(s)
