4eee88134de20015d05d37ab6e4075331d1b6f02
Yrnar <ernar030913@gmail.com>
2024-02-17 23:50:49 +0600

    Filter

--- Practice/myproject/myapp/views.py
+++ Practice/myproject/myapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 def student_list(request):
     students = ['Zhilkibaev Yernar', 'Ali Madiyar', 'Mauletkhan Nurbek', 'Zhumataev Ali', 'Eszhan Aliar','Saparov Aldiar', 'Dilda Ilyas', 'Mukhtarov Daniyal',]
-
-    return render(request, 'student_list.html', {'students': students})
+    initial = request.GET.get('initial')
+    filtered_students = []
+    if initial:
+        filtered_students = [student for student in students if initial.lower() in student.lower().split()[-1].lower()]
+    else:
+        filtered_students = students
+    
+    return render(request, 'student_list.html', {'students': filtered_students, 'initial': initial})
