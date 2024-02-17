7c7c41f31d100706bd567932b34cf3fa8f5cab6f
Nursultan Yerkenov <yerken1656@gmail.com>
2024-02-17 21:28:37 +0600

    add: pracitce_2(filter)

--- practice_1/app/views.py
+++ practice_1/app/views.py
@@ -1,15 +1,27 @@
+import dataclasses
+
 from django.shortcuts import render
 
-students = [
-    'Nursultan',
-    'Yersultan',
-    'Aliar',
-    'Bolat',
-    'Temirlan',
+
+@dataclasses.dataclass
+class User:
+    name: str
+    age: int
+    gender: str
+
+
+users = [
+    User("John", 27, "male"),
+    User("Jane", 66, "female"),
+    User("Mike", 19, "female"),
+    User("David", 8, "female"),
+    User("John", 33, "female"),
+    User("Mike", 5, "female"),
+    User("David", 78, "male"),
 ]
 
 
 def index(request):
     return render(request, 'index.html', {
-        'students': students
+        'users': users
     })
