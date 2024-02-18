a59d429e86a7bfadf9ece5da9f94da7ab6c29fc2
Yrnar <ernar030913@gmail.com>
2024-02-19 00:19:02 +0600

    Correct filter

--- Practice/myproject/myapp/views.py
+++ Practice/myproject/myapp/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 def student_list(request):
     students = ['Zhilkibaev Yernar', 'Ali Madiyar', 'Mauletkhan Nurbek', 'Zhumataev Ali', 'Eszhan Aliar','Saparov Aldiar', 'Dilda Ilyas', 'Mukhtarov Daniyal',]
-    initial = request.GET.get('initial')
-    filtered_students = []
-    if initial:
-        filtered_students = [student for student in students if initial.lower() in student.lower().split()[-1].lower()]
-    else:
-        filtered_students = students
+    modified_students = []
+    for student in students:
+        if student == 'Dilda Ilyas' or student == 'Mukhtarov Daniyal':
+            modified_students.append('Student not found')
+        else:
+            modified_students.append(student)
     
-    return render(request, 'student_list.html', {'students': filtered_students, 'initial': initial})
+    return render(request, 'student_list.html', {'students': modified_students})
