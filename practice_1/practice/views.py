52a6b2db0c9c5c5fa051aadfa184728fc3d7bb62
Gabdylgaziz <g_zhagypar@kbtu.kz>
2024-02-24 23:27:06 +0600

    practice_

--- practice_1/practice/views.py
+++ practice_1/practice/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def student_list(request):
     students = ['Student1', 'Student2', 'Student3', 'Student4', 'Student5', 'Student6', 'Student7', 'Student8', 'Student9', 'Student10'] 
     return render(request, 'students.html', {'students': students})
+
+def capitalizer(request):
+    return 
