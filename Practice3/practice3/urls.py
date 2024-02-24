cc8c1e8dcaa192e2dba1c757e3f6562b95e6a6b4
DaniyaRRa <d_raiymbekov@kbtu.kz>
2024-02-24 15:32:13 +0600

    Practice 2 Task

--- Practice3/practice3/urls.py
+++ Practice3/practice3/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from main.views import view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("hello/", include("hello.urls"))
+    path("", view, name = 'view')
 ]
--- kbtu_practice_14/lesson_1/lesson_1/urls.py
+++ kbtu_practice_14/lesson_1/lesson_1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for lesson_1 project.
+URL configuration for practice3 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
