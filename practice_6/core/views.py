a48bff3c2c84a909b7015ccffa37c6cb17e0db63
Karina <kmirova731@gmail.com>
2024-02-24 23:59:44 +0600

    practice week 6

---
+++ practice_6/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from core.forms import ProductForm
+
+# Create your views here.
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            raise Exception(f"Some Exception {form.errors}")
+        return HttpResponse(f'OK, product was created')
+    
+    return render(request, 'index.html', {'form': ProductForm()})
