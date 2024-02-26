5bbb9b4241af86361cad8803eca54bcd7c5518b0
Ali Zhumatayev <alizhumataeyev@Alis-MacBook-Air-2.local>
2024-02-27 00:07:57 +0600

    practiceForms

--- practice/week3/pythonProject/practice_week3_task/practice_week3_task/forms.py
+++ practice/week3/pythonProject/practice_week3_task/practice_week3_task/forms.py
@@ -4,7 +4,7 @@ from .models import MyModel
 
 class MyForm(forms.ModelForm):
     class Meta:
-        model = MyModel  # If you have a model to save form data, otherwise omit this line
-        fields = ['field1', 'field2']  # Define the fields you want in your form
+        model = MyModel
+        fields = ['field1', 'field2']
 
 
--- practice/week3/pythonProject/practice_week3_task/practice_week3_task/views.py
+++ practice/week3/pythonProject/practice_week3_task/practice_week3_task/views.py
@@ -15,8 +15,6 @@ def my_view(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
-            # Process the form data
-            # For demonstration purposes, you can save the form data to the database here
             form.save()
             return redirect('my_template.html')
     else:
