e1d4d136785893271e11262a58483de5e3e43c59
IskanderSaginbek <saginbekiskander@gmail.com>
2024-02-27 12:59:36 +0600

    fixed urls

--- practices/practice5/pr5/urls.py
+++ practices/practice5/pr5/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('/students', views.student),
-    path('/teachers', views.teacher),
-    path('/lessons', views.lesson),
-    path('/faculties', views.faculty),
+    path('students/', views.student),
+    path('teachers/', views.teacher),
+    path('lessons/', views.lesson),
+    path('faculties/', views.faculty),
 ]
