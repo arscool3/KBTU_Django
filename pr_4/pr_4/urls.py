e901d45dcee9d8ef9b4f41bad65978a3d589a177
Zhadi <zhadi000s@gmail.com>
2024-02-27 22:17:15 +0600

    пркатика 4 сделана мигом

--- pr_4/pr_4/urls.py
+++ pr_4/pr_4/urls.py
@@ -1,4 +1,4 @@
-"""pr_2 URL Configuration
+"""pr_4 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
