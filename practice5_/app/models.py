e635fbd0b55fa13f742eec3b66d80f2eac3339c1
orynbay <oryn.bay505@gmail.com>
2024-02-27 20:26:24 +0600

    practice5 Ospanova Aruzhan and Orynbay Aruzhan

---
+++ practice5_/app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Base(models.Model):
+    name = models.CharField(max_length=20)
+    class Meta:
+        abstract = True
+
+class Corporation(Base):
+    field = models.CharField(max_length=100)  
+    staff_number = models.IntegerField(blank=True, null=True)
+
+
+class Department(Base):
+    corporation = models.ForeignKey(Corporation, on_delete=models.CASCADE)
+    staff_number = models.IntegerField(blank=True, null=True)
+
+class Employee(Base):
+    position = models.CharField(max_length=100) 
+    salary = models.IntegerField(blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    Depratment = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')  
+
+class Project(Base):
+    budget=models.IntegerField(blank=True, null=True)
+    is_successful=models.BooleanField(default=False)
+    employee=models.ForeignKey(Employee,on_delete=models.PROTECT)
--- practice5_/practice5_/wsgi.py
+++ practice5_/practice5_/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for practice5 project.
+WSGI config for practice5_ project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice5_.settings')
 
 application = get_wsgi_application()
