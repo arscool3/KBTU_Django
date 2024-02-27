0dba29545bcb05a7e50ea6dd6e38bc5e92cfa722
/ Az1tchhh / <azamatbazarbai007@gmail.com>
2024-02-27 16:54:51 +0600

    Опять переименовал их, потому что они не запускались. Practice3Week - practice1#, Practice5Wekk - practice2#, Practice6Week - practice4#

--- kbtu_practice_13/practice5Week/practice5Week/wsgi.py
+++ kbtu_practice_13/practice5Week/practice5Week/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for practice6Week project.
+WSGI config for practice2# project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice6Week.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice2#.settings')
 
 application = get_wsgi_application()
