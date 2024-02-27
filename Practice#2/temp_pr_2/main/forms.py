aff21e3ca1272571235d7ec8d17a30ad824fe317
muktarkhanabilda <117581921+muktarkhanabilda@users.noreply.github.com>
2024-02-27 22:39:07 +0300

    pr_2

---
+++ Practice#2/temp_pr_2/main/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from main.models import Lesson, Student, Teacher
+
+class LessonForm(forms.ModelForm):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
