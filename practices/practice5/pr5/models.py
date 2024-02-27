c6fab17eaef8e02396c38a770b50c0487be432d0
IskanderSaginbek <saginbekiskander@gmail.com>
2024-02-27 12:50:19 +0600

    added practice5

---
+++ practices/practice5/pr5/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+class Person(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=50)
+    semester = models.CharField(max_length=50)
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=50)
+
+class StudentQuerySet(models.QuerySet):
+    def freshmen(self):
+        return self.filter(year=1)
+    def sophomores(self):
+        return self.filter(year=2)
+    def juniors(self):
+        return self.filter(year=3)
+    def seniors(self):
+        return self.filter(year=4)
+
+class StudentManager(models.Manager):
+    def get_queryset(self):
+        return StudentQuerySet(self.model, using=self._db)
+    def freshmen(self):
+        return self.get_queryset().freshmen()
+    def sophomores(self):
+        return self.get_queryset().sophomores()
+    def juniors(self):
+        return self.get_queryset().juniors()
+    def seniors(self):
+        return self.get_queryset().seniors()
+
+class TeacherQuerySet(models.QuerySet):
+    def filter_by_faculty(self,faculty):
+        return self.filter(faculty=faculty)
+    def high_ranking(self):
+        return self.filter(salary__gte=1000000)
+
+class TeacherManager(models.Manager):
+    def get_queryset(self):
+        return TeacherQuerySet(self.model, using=self._db)
+    def filter_by_faculty(self,faculty):
+        return self.get_queryset().filter_by_faculty(faculty)
+    def high_ranking(self):
+        return self.get_queryset().high_ranking()
+
+class Student(Person):
+    year = models.IntegerField()
+    faculty = models.ForeignKey('Faculty',on_delete=models.CASCADE)
+    lessons = models.ManyToManyField('Lesson')
+    objects = StudentManager()
+
+class Teacher(Person):
+    salary = models.IntegerField()
+    faculty = models.ForeignKey('Faculty',on_delete=models.CASCADE)
+    lesson = models.ForeignKey('Lesson',on_delete=models.CASCADE)
+    objects = TeacherManager()
