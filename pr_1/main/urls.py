c845addad82e8b423ab8f7e28d39e526d0b9e609
Zhadi <zhadi000s@gmail.com>
2024-02-27 21:28:42 +0600

    Практика 1 просто 3 вюшки

---
+++ pr_1/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('',home, name= home),
+    path('about/',about,name=about),
+    path('contact/',contact,name=contact)
+]
