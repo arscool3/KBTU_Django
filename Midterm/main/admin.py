dd487bbc05b0ee112fba6417c29922ad2d04248c
DaniyaRRa <d_raiymbekov@kbtu.kz>
2024-03-02 16:16:59 +0500

    Midterm Part2

--- Midterm/main/admin.py
+++ Midterm/main/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Item, Seller, City, Customer
+from .models import Airline, Aircraft, City, Airport, Flight_fact, Flight_dim
 
-admin.site.register(Item)
-admin.site.register(Seller)
+admin.site.register(Airline)
+admin.site.register(Aircraft)
 admin.site.register(City)
-admin.site.register(Customer)
+admin.site.register(Airport)
+admin.site.register(Flight_fact)
+admin.site.register(Flight_dim)
--- Midterm/midterm/urls.py
+++ Midterm/midterm/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for practice4 project.
+URL configuration for midterm project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import view, view2, view3, view4, view5
+from main.views import view
 
 urlpatterns = [
-    path("", view5, name = 'view5'),
+    path("", view, name = 'view'),
     path("admin/", admin.site.urls)
 ]
