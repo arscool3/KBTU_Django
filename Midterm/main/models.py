1e9f72b103247cc04cbdb3ef6746da9bd71f1de6
DaniyaRRa <d_raiymbekov@kbtu.kz>
2024-03-02 16:16:59 +0500

    Midterm Part2

--- Midterm/main/models.py
+++ Midterm/main/models.py
@@ -2,44 +2,69 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 import datetime
 
-class Aircraft():
-    aircraft_id = models.AutoField(primary_key=True)
-    aircraft_shortname = models.CharField(max_length=10)
-    aircraft_builtyear = models.IntegerField(validators=[MaxValueValidator(int(datetime.date.today().year)), MinValueValidator(1900)])
-    aircraft_capacity = models.IntegerField(validators=[MinValueValidator(1)])
 
-class CustomerQuerySet(models.QuerySet):
-    def filter_name_starting_with_D(self):
-        return self.filter(name = r'K.*')
+class Airline(models.Model):
+    airline_id = models.AutoField(primary_key=True)
+    airline_name = models.CharField(max_length=50)
+    airline_founded_date = models.DateField()
 
-class City():
-    city_id = models.AutoField(primary_key=True)
-    city_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.airline_name
+
+class Aircraft(models.Model):
+    id = models.AutoField(primary_key=True)
+    aircraft_code = models.CharField(max_length=10)
+    full_name = models.CharField(max_length=50)
+    short_name = models.CharField(max_length=10)
+    built_date = models.DateField()
+    full_capacity = models.IntegerField(validators=[MinValueValidator(1)])
+    economy_capacity = models.IntegerField(validators=[MinValueValidator(0)])
+    business_capacity = models.IntegerField(validators=[MinValueValidator(0)])
+    owner_airline = models.ForeignKey(Airline, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.short_name + ' (' + self.aircraft_code + ')'
+
+class City(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
     city_population = models.IntegerField(validators=[MinValueValidator(1)])
 
-class Airport():
+    def __str__(self):
+        return self.name
+
+class Airport(models.Model):
     airport_id = models.AutoField(primary_key=True)
-    city_name = models.ForeignKey(City, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
     airport_name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
-    is_international = models.BooleanField()
+    is_international = models.BooleanField(choices=[
+        (True, 'International'),
+        (False, 'Domestic'),
+    ])
 
-class Airline():
-    airline_id = models.AutoField(primary_key=True)
-    city_name = models.ForeignKey(City, on_delete=models.CASCADE)
-    address = models.CharField(max_length=50)
+    def __str__(self):
+        return self.city.name + ' ' + self.airport_name
+class Flight_fact(models.Model):
+    flight_code = models.CharField(max_length=10)
+    airline_name = models.ForeignKey(Airline, on_delete=models.CASCADE)
+    airport_from = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departures')
+    airport_to = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrivals')
 
-class Item():
-    item_id = models.IntegerField()
-    cost = models.FloatField()
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-    amount = models.IntegerField()
+    dept_time = models.TimeField()
+    arr_time = models.TimeField()
 
-class Orders():
-    order_id = models.IntegerField()
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    Seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-    Item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    order_date = models.DateTimeField(auto_now_add=True)
+    diff_days = models.IntegerField(validators=[MinValueValidator(-1), MaxValueValidator(1)])
 
-#
+    def __str__(self):
+        return self.flight_code
+
+class Flight_dim(models.Model):
+    flight_id = models.AutoField(primary_key=True)
+    flight_code = models.ForeignKey(Flight_fact, on_delete=models.CASCADE)
+    aircraft = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
+    flight_date = models.DateField()
+    is_sale_open = models.BooleanField()
+
+    def __str__(self):
+        return self.flight_code.flight_code + ' (' + f'{self.flight_date}' + ')'
--- Midterm/main/views.py
+++ Midterm/main/views.py
@@ -2,33 +2,9 @@ import dataclasses
 
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import StudentForm
-from .models import City, Customer, Seller, Item, Orders
-
-@dataclasses.dataclass
-class Student:
-    name: str
-    age: int
-    course: int
-
-
-students = [
-    Student(name='Anar', age=18, course=2),
-    Student(name='Dauren', age=19, course=2),
-    Student(name='Daniyar', age=20, course=3),
-]
-
+from .models import Airline, Aircraft, City, Airport, Flight_fact, Flight_dim
 
 def view(request):
-    return render(request, 'index.html', {'students': students})
+    return render(request, 'index.html')
 
-def view2(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        age = request.POST.get("age")
-        course = request.POST.get("course")
-        students.append(Student(name = name, age = age, course = course))
-        print('i am here')
-    studentform = StudentForm()
-    return render(request, 'index2.html', {'form': studentform})
 
