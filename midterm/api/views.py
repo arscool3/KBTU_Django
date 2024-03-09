238034f21dc443ea54f3b6ee09723aa0054d5476
/ Az1tchhh / <azamatbazarbai007@gmail.com>
2024-03-09 12:07:08 +0500

    configured prjoect

---
+++ midterm/api/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def test(request):
+    if request.method == 'GET':
+        return HttpResponse('test')
+
