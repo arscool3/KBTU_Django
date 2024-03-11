ee482772cb9e183b425724f951ec584f4e4cf12f
Ali Zhumatayev <alizhumataeyev@Alis-MacBook-Air-2.local>
2024-03-11 22:23:02 +0500

    midterm

--- midterm/midterm_project/main/views.py
+++ midterm/midterm_project/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
