897b551afc6be4a231b01a0e03d6a10870e0ee45
Azizbek <123943750+Azeke007@users.noreply.github.com>
2024-03-12 12:38:37 +0500

    Delete travel_planner directory

--- travel_planner/travel_app/urls.py
+++
@@ -1,9 +0,0 @@
-# travel_app/urls.py
-from django.urls import path
-from .views import destination_list, destination_detail
-
-urlpatterns = [
-    path('destinations/', destination_list, name='destination_list'),
-    path('destinations/<int:destination_id>/', destination_detail, name='destination_detail'),
-    # Add similar paths for Itineraries, Activities, User profiles, and Reviews
-]
