ff8f00b50e4e4d95dde5fcfb6b0dae46a4d87bce
Ayan <affection.ayan@mail.com>
2024-03-13 00:27:28 +0600

    Practice 1 and 2

---
+++ Practice_1/Practice_1/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for Practice_1 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Practice_1.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', Home_view),
+    path('about/', About_view),
+    path('profile/', Profile_view),
+]
--- Practice_1/djangoProject/djangoProject/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def student_list(request):
-    students = ['Ayan', 'Arsen', 'Arman', 'Almas']
-    return render(request, 'student_list.html', {'students': students})
