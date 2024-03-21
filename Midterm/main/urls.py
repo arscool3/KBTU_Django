f32a7c4821018fef1ec1407e4c709ea53de36dd3
DaniyaRRa <d_raiymbekov@kbtu.kz>
2024-03-21 20:04:27 +0500

    Midterm Final

--- Midterm/main/urls.py
+++ Midterm/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import get_flights, search_ticket, login_view, check_view, logout_view, register_view, entry_page
+from .views import get_flights, search_ticket, login_view, logout_view, register_view, entry_page, admin_view, aircraft_time_scheduling
 
 urlpatterns = [
     path("get_flights/", get_flights, name="get_flights"),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", entry_page, name="entry_page"),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
-    path('check/', check_view, name='check'),
-    path("logout/", logout_view, name='logout')
+    path("logout/", logout_view, name='logout'),
+    path("admin_page/", admin_view, name='admin_page'),
+    path("aircraft_time_scheduling/", aircraft_time_scheduling, name='aircraft_time_scheduling')
 ]
