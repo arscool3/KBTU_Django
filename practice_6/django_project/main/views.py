6265db286fbd1c5902249721eae4f4e4249491c7
Bekarys Nurgasym <bekarysnurgassym@gmail.com>
2024-03-30 17:22:08 +0500

    practice 6 | views and urls

---
+++ practice_6/django_project/main/views.py
@@ -0,0 +1,173 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category, Author, Book, Consumer, Review
+from .serializers import CategorySerializer, AuthorSerializer, BookSerializer, ReviewSerializer, ConsumerSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class UserListView(APIView):
+  def get(self, request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+class CategoryListView(APIView):
+  def get(self, request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
+
+class BookListView(APIView):
+  def get(self, request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+class BookDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request, book_id):
+        book = Book.objects.get(id=book_id)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)  
+
+class AuthorListView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+class ConsumerListView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request):
+        consumers = Consumer.objects.all()
+        serializer = ConsumerSerializer(consumers, many=True)
+        return Response(serializer.data)
+
+class ReviewListView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request):
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+      
+class RegisterUserView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({"error": "Username, email, and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if the username or email already exists
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Username or email already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Create the user
+        user = User.objects.create_user(username=username, password=password)
+
+        # Generate JWT tokens
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        serializer = UserSerializer(user)
+        return Response({"user": serializer.data, "access_token": access_token}, status=status.HTTP_201_CREATED)
+      
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({"error": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Generate JWT tokens
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+
+        return Response({"access_token": access_token}, status=status.HTTP_200_OK)
+      
+class AddCategoryView(APIView):
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AddReviewView(APIView):
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        serializer = ReviewSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AddAuthorView(APIView):
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        serializer = AuthorSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AddBookView(APIView):
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AddConsumerView(APIView):
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        serializer = ConsumerSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
