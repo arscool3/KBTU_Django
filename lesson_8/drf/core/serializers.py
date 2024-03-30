8240b5a6e42b8cfb050152d364b99733fb5a3b2f
Arslan Yersain <arslanersain@gmail.com>
2024-03-30 12:21:03 +0500

    lesson 8

---
+++ lesson_8/drf/core/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from core.models import Weather
+
+class WeatherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weather
+        fields = '__all__'
+
