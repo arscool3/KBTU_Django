8a3216591c784f88dc64996721f1d4486e8d58bc
Bekarys Nurgasym <bekarysnurgassym@gmail.com>
2024-03-30 14:44:28 +0500

    practice 6 | serializers added

--- django_project/main/urls.py
+++ django_project/main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-
+from rest_framework import routers
 from main.views import index, login_view, logout_view, users, authors, books, categories, consumers, reviews, get_book
+from main.views2 import CategoryViewSet
+
+router = routers.SimpleRouter()
+
+router.register(r'category', CategoryViewSet)
 
 urlpatterns = [
   path('', index, name='index'),
@@ -10,9 +15,9 @@ urlpatterns = [
   # Get methods
   path('users/', users, name='users'),
   path('authors/', authors, name='authors'),
-  path('categories/', categories, name='categories'),
+  # path('categories/', categories, name='categories'),
   path('books/', books, name='books'),
   path('books/<int:id>', get_book, name='get_book'),
   path('consumers/', consumers, name='consumers'),
   path('reviews/', reviews, name='reviews'),
-]
+] + router.urls
