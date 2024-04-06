6c16b2b7bcd9d990e205a71f95250d491338e3f0
Dias21B30874 <d_mombekov@kbtu.kz>
2024-04-06 11:46:33 +0500

    practice 6

--- midterm/shiki/urls.py
+++ midterm/shiki/urls.py
@@ -4,21 +4,16 @@ from .views import *
 app_name = "shiki"
 
 urlpatterns = [
-    path('anime/create/', anime_create, name='anime_create'),
-    path('manga/create/', manga_create, name='manga_create'),
-    path('light_novel/create/', light_novel_create, name='light_novel_create'),
-    path('genres/create/', genre_create, name='genre_create'),
-    path('genres/', genre_list, name='genre_list'),
-    path('genres/<int:genre_id>/', genre_read, name='genre_read'),
-    path('genres/<int:genre_id>/update/', genre_update, name='genre_update'),
-    path('genres/<int:genre_id>/delete/', genre_delete, name='genre_delete'),
-    path('signup/', signup, name='signup'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logged_out'),
-    path('anime/', anime_list, name='anime_list'),
-    path('anime/<int:anime_id>/', anime_read, name='anime_read'),
-    path('anime/<int:anime_id>/update/', anime_update, name='anime_update'),
-    path('anime/<int:anime_id>/delete/', anime_delete, name='anime_delete'),
-    path('profile/', profile, name='profile'),
+    path('anime/', anime_list, name='anime-list'),
+    path('anime/<int:pk>/', anime_detail, name='anime-detail'),
+    path('anime/create/', anime_create, name='anime-create'),
+    path('anime/<int:pk>/update/', anime_update, name='anime-update'),
+    path('anime/<int:pk>/delete/', anime_delete, name='anime-delete'),
+
+    path('genre/', genre_list, name='genre-list'),
+    path('genre/<int:pk>/', genre_detail, name='genre-detail'),
+    path('genre/create/', genre_create, name='genre-create'),
+    path('genre/<int:pk>/update/', genre_update, name='genre-update'),
+    path('genre/<int:pk>/delete/', genre_delete, name='genre-delete'),
 
 ]
