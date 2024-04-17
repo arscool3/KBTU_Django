c86d19bbbd4370f66120f1e4bd747d3c8c875136
Toktamys Gulim <118511119+guliiim@users.noreply.github.com>
2024-04-17 20:50:33 +0500

    Add files via upload

---
+++ lecture8_practice/core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework import routers
+from core.views import CosmeticProductViewSet
+
+router = routers.SimpleRouter()
+router.register(r"cosmetic-products", CosmeticProductViewSet)
+urlpatterns = [] + router.urls
+
+
+
+
