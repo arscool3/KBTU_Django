1e8864c7db4dd3b3712ee882f83b946082e74c0e
aalmatmm <a_madiyarbek@kbtu.kz>
2024-04-28 18:08:57 +0500

    prac 9

--- practice8/main.py
+++ practice8/main.py
@@ -1,18 +1,67 @@
-import dramatiq
-from dramatiq.brokers.redis import RedisBroker
-from dramatiq.results import Results
-from dramatiq.results.backends.redis import RedisBackend
-from gov import crime_service, psycho_service, drugs_service
-
-result_backend = RedisBackend()
-redis_broker = RedisBroker()
-redis_broker.add_middleware(Results(backend=result_backend))
-dramatiq.set_broker(redis_broker)
+from datetime import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 
-@dramatiq.actor(store_results=True)
-def ask_papers_score(paper_id):
-    import time
-    time.sleep(5) # exammple delay for getting data
-    academic_scores = {"paper_id": paper_id, "score": 4.9}
-    return academic_scores
+app = FastAPI()
+
+shops = []
+products = []
+
+class Book(BaseModel):
+    name: str
+    author: str
+
+class Category(BaseModel):
+    name: str
+    book_id: int
+
+books = []
+
+categories = []
+
+@app.post("/books/")
+async def create_book(book: Book):
+    books.append(book)
+    return {"message": "book created successfully"}
+
+@app.get("/books/", response_model=List[Book])
+async def get_books():
+    return books
+
+@app.get("/books/{index}", response_model=Book)
+async def get_book_by_index(index: int):
+    try:
+        return books[index]
+    except IndexError:
+        raise HTTPException(status_code=404, detail="book not found")
+
+@app.put("/books/{index}")
+async def update_book(index: int, book: Book):
+    try:
+        books[index] = book
+        return {"message": "book updated successfully"}
+    except IndexError:
+        raise HTTPException(status_code=404, detail="book not found")
+
+@app.delete("/books/{index}")
+async def delete_book(index: int):
+    try:
+        del books[index]
+        return {"message": "book deleted successfully"}
+    except IndexError:
+        raise HTTPException(status_code=404, detail="book not found")
+
+@app.post("/categories/")
+async def create_category(category: Category):
+    categories.append(category)
+    return {"message": "Category created successfully"}
+
+@app.get("/categories/", response_model=List[Category])
+async def get_categories():
+    return categories
+
+@app.get("/books/{book_id}/categories", response_model=List[Category])
+async def get_categories_for_book(book_id: int):
+    return [category for category in categories if category.book_id == book_id]
--- practice9/main.py
+++ practice9/main.py
@@ -1,67 +1,33 @@
-from datetime import datetime
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from pydantic import BaseModel
-from typing import List
-
+from typing import Optional
 
 app = FastAPI()
+database = "Fake Database"
 
-shops = []
-products = []
+def get_db():
+    return database
 
-class Book(BaseModel):
-    name: str
-    author: str
+@app.get("/")
+def read_root(db: str = Depends(get_db)):
+    return {"message": f"Connected to {db}"}
 
-class Category(BaseModel):
-    name: str
-    book_id: int
+class Database:
+    def __init__(self):
+        self.db = "Fake Database"
 
-books = []
+    def get_db(self):
+        return self.db
 
-categories = []
+db_instance = Database()
 
-@app.post("/books/")
-async def create_book(book: Book):
-    books.append(book)
-    return {"message": "book created successfully"}
+@app.get("/db")
+def read_db(db: str = Depends(db_instance.get_db)):
+    return {"message": f"Connected to {db}"}
 
-@app.get("/books/", response_model=List[Book])
-async def get_books():
-    return books
+def create_database():
+    return "Fake Database"
 
-@app.get("/books/{index}", response_model=Book)
-async def get_book_by_index(index: int):
-    try:
-        return books[index]
-    except IndexError:
-        raise HTTPException(status_code=404, detail="book not found")
-
-@app.put("/books/{index}")
-async def update_book(index: int, book: Book):
-    try:
-        books[index] = book
-        return {"message": "book updated successfully"}
-    except IndexError:
-        raise HTTPException(status_code=404, detail="book not found")
-
-@app.delete("/books/{index}")
-async def delete_book(index: int):
-    try:
-        del books[index]
-        return {"message": "book deleted successfully"}
-    except IndexError:
-        raise HTTPException(status_code=404, detail="book not found")
-
-@app.post("/categories/")
-async def create_category(category: Category):
-    categories.append(category)
-    return {"message": "Category created successfully"}
-
-@app.get("/categories/", response_model=List[Category])
-async def get_categories():
-    return categories
-
-@app.get("/books/{book_id}/categories", response_model=List[Category])
-async def get_categories_for_book(book_id: int):
-    return [category for category in categories if category.book_id == book_id]
+@app.get("/create_db")
+def read_create_db(db: str = Depends(create_database)):
+    return {"message": f"Connected to {db}"}
