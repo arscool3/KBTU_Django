5f9972d91254f1dfa986f1b06414e12f7937e997
Dias21B30874 <d_mombekov@kbtu.kz>
2024-04-28 13:50:54 +0500

    Practice10

---
+++ Practice10V2/main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+
+# Models
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+
+
+class Order(BaseModel):
+    id: int
+    user_id: int
+    item_id: int
+    quantity: int
+
+
+# Dummy data
+users = [
+    User(id=1, username="Dias", email="dias@example.com"),
+    User(id=2, username="Dias2", email="dias2@example.com"),
+    User(id=3, username="Dias3", email="dias3@example.com"),
+]
+
+items = [
+    Item(id=1, name="milk", description="whire"),
+    Item(id=2, name="phone", description="white"),
+    Item(id=3, name="tv", description="big"),
+]
+
+orders = [
+    Order(id=1, user_id=1, item_id=1, quantity=2),
+    Order(id=2, user_id=2, item_id=2, quantity=3),
+    Order(id=3, user_id=3, item_id=3, quantity=1),
+]
+
+
+# Routes
+@app.get("/users/", response_model=List[User])
+async def get_users():
+    return users
+
+
+@app.post("/users/")
+async def create_user(user: User):
+    users.append(user)
+    return user
+
+
+@app.get("/items/", response_model=List[Item])
+async def get_items():
+    return items
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    items.append(item)
+    return item
+
+
+@app.get("/orders/", response_model=List[Order])
+async def get_orders():
+    return orders
+
+
+@app.post("/orders/")
+async def create_order(order: Order):
+    orders.append(order)
+    return order
