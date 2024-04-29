5ec0c5e240f915d1655b8cda38f1892828302e70
Az1tchhh <azamatbazarbai007@gmail.com>
2024-04-29 19:08:13 +0500

    done

--- endterm/fastApiProject/main.py
+++ endterm/fastApiProject/main.py
@@ -54,6 +54,7 @@ airport_log_filename = "airport_log"
 plane_log_filename = "plane_log"
 flight_log_filename = "flight_log"
 ticket_log_filename = "ticket_log"
+user_log_filename = "user_log"
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -106,17 +107,23 @@ def create_log(filename: str, username: str, method, message: str):
                                                  f"MESSAGE: {message}")
 # User registration endpoint
 @app.post("/register", response_model=schemas.User, tags=["users"])
-async def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def register(request: Request, user: schemas.UserCreate, db: Session = Depends(get_db)):
+    message = f"SOME USER TRYING TO REGISTER. REGISTRATION DATA: {user.__str__()}"
+    create_log(user_log_filename, user.username, request.method, message)
     db_user = user_crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
+    message = f"{user.username} REGISTERED SUCCESSFULLY"
+    create_log(user_log_filename, user.username, request.method, message)
     return user_crud.create_user(db=db, user=user)
 
 
 # Login endpoint
 @app.post("/token", response_model=Token, tags=["users"])
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+async def login(request: Request, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = user_crud.authenticate_user(db, form_data.username, form_data.password)
+    message = f"{user.username} IS TRYING TO LOG IN"
+    create_log(user_log_filename, user.username, request.method, message)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     access_token_expires = timedelta(minutes=user_crud.ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -128,25 +135,25 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = 
         data={"sub": user.username}, expires_delta=refresh_token_expires
     )
     result = Token(access_token=access_token, refresh_token=refresh_token)
+    message = f"{user.username} SUCCESSFULLY LOGGED IN"
+    create_log(user_log_filename, user.username, request.method, message)
     return result
 
 
 @app.get("/countries/", response_model=list[schemas.Country], tags=["country"])
 def read_countries(request: Request, skip: int = 0, limit: int = 10, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING ALL COUNTRIES"
-    method = request.method
-    create_log(country_log_filename, username, method, message)
     countries = country_crud.get_countries(db, skip=skip, limit=limit)
+    create_log(country_log_filename, username, request.method, message)
     return countries
 
 @app.get("/countries/{country_id}", response_model=schemas.Country, tags=["country"])
 def read_country(request: Request, country_id: int, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING THE COUNTRY WITH ID: {country_id}"
-    method = request.method
-    create_log(country_log_filename, username, method, message)
     country = country_crud.get_country(db, country_id=country_id)
     if country is None:
         raise HTTPException(status_code=404, detail="Country not found")
+    create_log(country_log_filename, username, request.method, message)
     return country
 
 @app.post("/countries/", response_model=schemas.Country, tags=["country"])
@@ -175,17 +182,17 @@ def delete_country(request: Request, country_id: int, db: Session = Depends(get_
 @app.get("/cities/", response_model=list[schemas.City], tags=["city"])
 def read_cities(request: Request, skip: int = 0, limit: int = 10, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING CITIES"
-    create_log(city_log_filename, username, request.method, message)
     cities = city_crud.get_cities(db, skip=skip, limit=limit)
+    create_log(city_log_filename, username, request.method, message)
     return cities
 
 @app.get("/cities/{city_id}", response_model=schemas.City, tags=["city"])
 def read_city(request: Request, city_id: int, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING A CITY WITH ID: {city_id}"
-    create_log(city_log_filename, username, request.method, message)
     city = city_crud.get_city(db, city_id=city_id)
     if city is None:
         raise HTTPException(status_code=404, detail="CITY NOT FOUND")
+    create_log(city_log_filename, username, request.method, message)
     return city
 
 @app.post("/cities/", response_model=schemas.City, tags=["city"])
@@ -211,26 +218,26 @@ def delete_city(request: Request, city_id: int, db: Session = Depends(get_db), u
 @app.get("/airports/", response_model=list[schemas.Airport], tags=["airport"])
 def read_airports(request: Request, skip: int = 0, limit: int = 10, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING AIRPORTS"
-    create_log(airport_log_filename, username, request.method, message)
     airports = airport_crud.get_airports(db, skip=skip, limit=limit)
+    create_log(airport_log_filename, username, request.method, message)
     return airports
 
 @app.get("/airports/{airport_id}", response_model=schemas.Airport, tags=["airport"])
 def read_airport(request: Request, airport_id: int, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING AN AIRPORT WITH ID: {airport_id}"
-    create_log(airport_log_filename, username, request.method, message)
     airport = airport_crud.get_airport(db, airport_id=airport_id)
     if airport is None:
         raise HTTPException(status_code=404, detail="AIRPORT NOT FOUND")
+    create_log(airport_log_filename, username, request.method, message)
     return airport
 
 @app.get("/airports/country/{country_id}", response_model=list[schemas.Airport], tags=["airport"])
 def read_airports_by_country(request: Request, skip: int = 0, limit: int = 10, country_id: int = None, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING AIRPORTS IN COUNTRY: {country_id}"
-    create_log(airport_log_filename, username, request.method, message)
     airports = airport_crud.get_airports(db, skip, limit, country_id)
     if airports is None:
         raise HTTPException(status_code=404, detail="AIRPORTS NOT FOUND")
+    create_log(airport_log_filename, username, request.method, message)
     return airports
 
 
@@ -256,18 +263,18 @@ def delete_airport(request: Request, airport_id: int, db: Session = Depends(get_
 @app.get("/planes/{plane_id}", response_model=schemas.Plane, tags=["plane"])
 def read_plane(request: Request, plane_id: int, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING A PLANE WITH ID: {plane_id}"
-    create_log(plane_log_filename, username, request.method, message)
     plane = plane_crud.get_plane(db, plane_id)
     if plane is None:
         raise HTTPException(status_code=404, detail="PLANE NOT FOUND")
+    create_log(plane_log_filename, username, request.method, message)
     return plane
 
 # Endpoint to get a list of planes
 @app.get("/planes/", response_model=list[schemas.Plane], tags=["plane"])
 def read_planes(request: Request, skip: int = 0, limit: int = 10, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING PLANES"
-    create_log(plane_log_filename, username, request.method, message)
     planes = plane_crud.get_planes(db, skip=skip, limit=limit)
+    create_log(plane_log_filename, username, request.method, message)
     return planes
 
 # Endpoint to create a new plane
@@ -285,6 +292,8 @@ def update_plane(request: Request, plane_id: int, plane: schemas.PlaneUpdate, db
     updated_plane = plane_crud.update_plane(db=db, plane_id=plane_id, plane=plane)
     if updated_plane is None:
         raise HTTPException(status_code=404, detail="PLANE NOT FOUND")
+    message = f"{username} UPDATED A PLANE WITH ID: {plane_id} -> {plane.__str__()}"
+    create_log(plane_log_filename, username, request.method, message)
     return updated_plane
 
 # Endpoint to delete a plane
@@ -295,6 +304,8 @@ def delete_plane(request: Request, plane_id: int, db: Session = Depends(get_db),
     deleted_plane = plane_crud.delete_plane(db=db, plane_id=plane_id)
     if deleted_plane is None:
         raise HTTPException(status_code=404, detail="PLANE NOT FOUND")
+    message = f"{username} IS DELETED A PLANE WITH ID: {plane_id}"
+    create_log(plane_log_filename, username, request.method, message)
     return deleted_plane
 
 @app.get("/flights/", response_model=list[schemas.Flight], tags=["flight"])
@@ -309,10 +320,10 @@ def read_flights(request: Request, skip: int = 0, limit: int = 10, db: Session =
 @app.get("/flights/{flight_id}", response_model=schemas.Flight, tags=["flight"])
 def read_flight(request: Request, flight_id: int, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
     message = f"{username} IS READING A FLIGHT WITH ID: {flight_id}"
-    create_log(flight_log_filename, username, request.method, message)
     flight = flight_crud.get_flight(db, flight_id=flight_id)
     if flight is None:
         raise HTTPException(status_code=404, detail="FLIGHT NOT FOUND")
+    create_log(flight_log_filename, username, request.method, message)
     return flight
 
 @app.post("/flights/", response_model=schemas.Flight, tags=["flight"])
@@ -328,6 +339,8 @@ def update_flight(request: Request, flight_id: int, flight: schemas.FlightUpdate
     updated_flight = flight_crud.update_flight(db=db, flight_id=flight_id, flight=flight)
     if updated_flight is None:
         raise HTTPException(status_code=404, detail="FLIGHT NOT FOUND")
+    message = f"{username} UPDATED A FLIGHT WITH ID: {flight_id} -> {flight.__str__()}"
+    create_log(flight_log_filename, username, request.method, message)
     return updated_flight
 
 @app.delete("/flights/{flight_id}", response_model=schemas.Flight, tags=["flight"])
@@ -337,25 +350,27 @@ def delete_flight(request: Request, flight_id: int, db: Session = Depends(get_db
     deleted_flight = flight_crud.delete_flight(db=db, flight_id=flight_id)
     if deleted_flight is None:
         raise HTTPException(status_code=404, detail="FLIGHT NOT FOUND")
+    message = f"{username} DELETED A FLIGHT WITH ID: {flight_id}"
+    create_log(flight_log_filename, username, request.method, message)
     return deleted_flight
 
 @app.get("/tickets/{ticket_id}", response_model=schemas.Ticket, tags=["ticket"])
 def read_ticket(request: Request, ticket_id: int, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
-    message = f"{username} IS READING A TICKET WITH ID: {ticket_id}"
-    create_log(ticket_log_filename, username, request.method, message)
     db_ticket = ticket_crud.get_ticket(db=db, ticket_id=ticket_id)
     if db_ticket is None:
         raise HTTPException(status_code=404, detail="Ticket not found")
+    message = f"{username} IS READING A TICKET WITH ID: {ticket_id}"
+    create_log(ticket_log_filename, username, request.method, message)
     return db_ticket
 
 # Get all tickets
 @app.get("/tickets/", response_model=list[schemas.Ticket], tags=["ticket"])
 def read_tickets(request: Request, skip: int = 0, limit: int = 10, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
-    message = f"{username} IS READING TICKETS"
-    create_log(ticket_log_filename, username, request.method, message)
     tickets = ticket_crud.get_tickets(db=db, skip=skip, limit=limit)
     if tickets is None:
         raise HTTPException(status_code=404, detail="Ticket not found")
+    message = f"{username} IS READING TICKETS"
+    create_log(ticket_log_filename, username, request.method, message)
     return tickets
 
 @app.post("/tickets/", response_model=schemas.Ticket, tags=["ticket"])
@@ -372,6 +387,8 @@ def update_ticket(request: Request, ticket_id: int, ticket: schemas.TicketCreate
     updated_ticket = ticket_crud.update_ticket(db=db, ticket_id=ticket_id, ticket=ticket)
     if updated_ticket is None:
         raise HTTPException(status_code=404, detail="Ticket not found")
+    message = f"{username} UPDATED A TICKET: {ticket_id} -> {ticket.__str__()}"
+    create_log(ticket_log_filename, username, request.method, message)
     return updated_ticket
 
 # Delete a ticket
@@ -382,20 +399,25 @@ def delete_ticket(request: Request, ticket_id: int, db: Session = Depends(get_db
     deleted_ticket = ticket_crud.delete_ticket(db=db, ticket_id=ticket_id)
     if deleted_ticket is None:
         raise HTTPException(status_code=404, detail="Ticket not found")
+    message = f"{username} DELETED A TICKET: {ticket_id}"
+    create_log(ticket_log_filename, username, request.method, message)
     return deleted_ticket
 
 @app.get("/tickets/available/", response_model=list[schemas.Ticket], tags=["ticket"])
 def read_available_tickets(request: Request, plane_name: str = "", skip: int = 0, limit: int = 10, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
-    message = f"{username} IS READING AVAILABLE TICKETS"
-    create_log(ticket_log_filename, username, request.method, message)
     tickets = ticket_crud.get_tickets_available(db=db, skip=skip, limit=limit, plane_name=plane_name)
     if tickets is None:
         raise HTTPException(status_code=404, detail="Tickets not found")
+    message = f"{username} IS READING AVAILABLE TICKETS"
+    create_log(ticket_log_filename, username, request.method, message)
     return tickets
 
 @app.post("/tickets/reserve", response_model=schemas.Ticket, tags=["ticket"])
 def reserve_ticket(request: Request, ticket_id: int, db: Session = Depends(get_db), username: str = Depends(TokenVerifier.verify_token)):
+    message = f"{username} IS RESERVING A FLIGHT"
+    create_log(ticket_log_filename, username, request.method, message)
     ticket: schemas.Ticket = ticket_crud.reserve_ticket(db=db, ticket_id=ticket_id)
+
     if ticket is None:
         raise HTTPException(status_code=404, detail="Ticket not found")
     if ticket == "reserved":
@@ -403,6 +425,6 @@ def reserve_ticket(request: Request, ticket_id: int, db: Session = Depends(get_d
     departure_airport_id = flight_crud.get_flight(db, ticket.flight_id).departure_airport_id
     destination_airport_id = flight_crud.get_flight(db, ticket.flight_id).destination_airport_id
     flight = f"FLIGHT ID: {ticket.flight_id} | DEPARTURE: {airport_crud.get_airport(db, departure_airport_id).name} | DESTINATION: {airport_crud.get_airport(db, destination_airport_id).name}"
-    message = f"{username} IS RESERVING A FLIGHT: {flight}; PLANE: {ticket.plane_id}; SEAT_NUMBER: {ticket.seat_number}"
+    message = f"{username} RESERVED A FLIGHT: {flight}; PLANE: {ticket.plane_id}; SEAT_NUMBER: {ticket.seat_number}; TICKET: {ticket.id}"
     create_log(ticket_log_filename, username, request.method, message)
     return ticket
