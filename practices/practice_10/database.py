1dad0e44ef7f407df004361a6c11f2680777b599
Nursultan Yerkenov <yerken1656@gmail.com>
2024-04-30 11:55:36 +0500

    add practices

---
+++ practices/practice_10/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
+url = 'postgresql://postgres:mycode@localhost/postgres'
+engine = create_engine(url)
+session = Session(engine)
+
+Base = declarative_base()
