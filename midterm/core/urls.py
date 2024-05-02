c71aff5cbf5c768bb1d034c9dddd21ca775cf93c
Karina <kmirova731@gmail.com>
2024-05-03 00:39:57 +0500

    upd fixed

--- midterm/core/urls.py
+++ midterm/core/urls.py
@@ -9,14 +9,13 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/student/', register_student_view, name='register_student'),
     path('register/instructor/', register_instructor_view, name='register_instructor'),
-    path('instructor/course/add/', instructor_create_course, name='add_course'),
-    path('student/course/add', student_add_course, name='student_add_course'),
+    path('student/courses/', student_courses, name='student_courses'),
     path('instructor/course/assignment/create', instructor_create_assignment, name='create_assignment'),
     #GET
     path('course/all/', get_all_courses, name='all_courses'),
     path('student/all/', get_all_students, name='all_students'),
-    path('student/courses/', get_student_courses, name='student_courses'),
-    path('instructor/all/', get_all_instructors, name='all_instuctors'),
-    path('instructor/courses/', get_instructor_courses, name='instructor_courses'),
+    path('instructor/courses/', instructor_courses, name='instructor_courses'),
+    path('instructor/all/', get_all_instructors, name='all_instuctors'),    
     path('user/', get_user, name='get_user'),
+    path('', get_all_courses, name='all_courses')
 ]
