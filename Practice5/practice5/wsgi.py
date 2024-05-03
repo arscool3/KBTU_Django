f9582d2a14dfe26ebc16751997d11315475c61bf
DaniyarRa <d_raiymbekov@kbtu.kz>
2024-05-04 02:33:28 +0500

    Practice 11

--- Practice5/practice5/wsgi.py
+++ Practice5/practice5/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for practice4 project.
+WSGI config for practice5 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practice4.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practice5.settings")
 
 application = get_wsgi_application()
