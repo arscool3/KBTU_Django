3015610eb4cf8ec9ea7d0e92b0dc2d65978bd6c0
Ali Zhumatayev <alizhumataeyev@Alis-MacBook-Air-2.local>
2024-05-04 21:58:16 +0500

    practice 8

---
+++ practice 8/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+tests = []
+
+
+class TestData(BaseModel):
+    x: int
+
+
+@app.post("/number/")
+async def create_test(test_data: TestData):
+    tests.append(test_data)
+    return {"message": "number created successfully"}
+
+
+@app.get("/number/")
+async def get_tests():
+    return tests
+
+
+@app.get("/number/{number_id}")
+async def get_test(test_id: int):
+    try:
+        return tests[test_id]
+    except:
+        raise HTTPException(status_code=404, detail="NOT FOUND!")
+
+
+@app.put("/number/{number_id}")
+async def update_test(test_id: int, test_data: TestData):
+    if test_id < 0 or test_id >= len(tests):
+        raise HTTPException(status_code=404, detail="number not found")
+    tests[test_id] = test_data
+    return {"message": "number updated successfully"}
+
+
+@app.delete("/number/{number_id}")
+async def delete_test(test_id: int):
+    try:
+        del tests[test_id]
+    except:
+        raise HTTPException(status_code=404, detail="number not found")
+    return {"message": "number deleted successfully"}
