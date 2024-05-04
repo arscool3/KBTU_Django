9c89e7bd206b2993a89b1b8f8134d64cb2debc33
Islam Aip <aipislam03@gmail.com>
2024-05-04 22:05:09 +0500

    removed duplicated prac4

--- practice_models/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from main.models import Teacher, Student, University, Course
-# Register your models here.
-
-admin.site.register(Teacher)
-admin.site.register(Student)
-admin.site.register(University)
-admin.site.register(Course)
