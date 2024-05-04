d91c75bb7ffcade8523da143c5ee7d0ea1ae6808
Islam Aip <aipislam03@gmail.com>
2024-05-04 21:50:35 +0500

    practice 4

---
+++ practice_4/main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from main.models import Teacher, Student, University, Course
+# Register your models here.
+
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(University)
+admin.site.register(Course)
