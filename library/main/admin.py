43e998055722f80cfc0ef1f8e07833b461cc4aea
Islam Aip <aipislam03@gmail.com>
2024-05-04 22:07:35 +0500

    removed duplicated group_project

--- library/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from main.models import Book, Author, Library, Client
-
-
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Library)
-admin.site.register(Client)
-# Register your models here.
