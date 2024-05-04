7fa9b69917142dfa23c6c1da652df2f32d5efa9c
muktarkhanabilda <117581921+muktarkhanabilda@users.noreply.github.com>
2024-05-04 12:53:46 +0600

    muktarkhan

---
+++ Practice#6/myblog/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
