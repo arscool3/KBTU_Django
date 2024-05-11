758ac440b90d77b24c2cd759840359c3c8603eed
IsakGairatov <isak3141592653@gmail.com>
2024-05-11 16:54:55 +0500

    added actions

--- Final/vk/api/views.py
+++ Final/vk/api/views.py
@@ -1,13 +1,29 @@
-
+from django.http import JsonResponse
 from rest_framework.viewsets import ModelViewSet
 from .serializers import *
 from app.models import Post, UserInfo, Image, Comment, Like, Group, Subscription
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Create your views here.
 
 
 class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    lookup_field = 'id'
+
+    @action(detail=True, methods=['get'])
+    def likeAmount(self, request, id: int):
+        return Response(Like.objects.filter(post=id).count())
+
+    @action(detail=True, methods=['get'])
+    def commentAmount(self, request, id: int):
+        return Response(Comment.objects.filter(post=id).count())
+
+    @action(detail=True, methods=['get'])
+    def postComments(self, request, id: int):
+        coms = CommentSerializer(Comment.objects.filter(post=id), many=True)
+        return Response(coms.data)
 
 
 class UserInfoViewSet(ModelViewSet):
@@ -28,6 +44,7 @@ class CommentViewSet(ModelViewSet):
 class LikeViewSet(ModelViewSet):
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
+
 
 
 class GroupViewSet(ModelViewSet):
