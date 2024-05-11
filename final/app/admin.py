2ef2ee584e34965aa9ec235a676c5b7e7a6c7ce8
Izetta <izettaionni@mail.ru>
2024-05-11 22:48:06 +0500

    refactored models and admin

--- final/app/admin.py
+++ final/app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Worker, Customer, Stock, Product, ProductsInStock, Order, Delivery
+
+
+class WorkerInline(admin.StackedInline):
+    model = Worker
+    can_delete = False
+    verbose_name_plural = "worker"
+
+
+class CustomerInline(admin.StackedInline):
+    model = Customer
+    can_delete = False
+    verbose_name_plural = "customer"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [WorkerInline, CustomerInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Stock)
+admin.site.register(Product)
+admin.site.register(ProductsInStock)
+admin.site.register(Order)
+admin.site.register(Delivery)
--- final/app/models.py
+++ final/app/models.py
@@ -5,12 +5,16 @@ from django.contrib.auth.models import User
 class Worker(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    objects = models.Manager()
+
     def __str__(self):
         return self.user.__str__()
 
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.user.__str__()
@@ -21,6 +25,8 @@ class Stock(models.Model):
     address = models.CharField(max_length=64)
     area = models.IntegerField(help_text="Area in square meters")
 
+    objects = models.Manager()
+
     def __str__(self):
         return self.name
 
@@ -29,50 +35,49 @@ class Product(models.Model):
     name = models.CharField(max_length=64)
     vendor = models.CharField(max_length=64)
     vendor_code = models.CharField(max_length=16, unique=True)
+    stock = models.ManyToManyField(Stock, through='ProductsInStock')
+
+    objects = models.Manager()
 
     def __str__(self):
         return str(self.name) + " " + str(self.vendor_code)
 
 
 class ProductsInStock(models.Model):
-    stock = models.OneToOneField(Stock, on_delete=models.CASCADE)
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+
+    class Meta:
+        unique_together = ('stock', 'product')
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.product.__str__() + " " + self.stock.__str__() + " " + str(self.quantity)
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, editable=False)
-    stock = models.ForeignKey(Stock, on_delete=models, editable=False)
-    ordered_datetime = models.DateTimeField(auto_now_add=True, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    ordered_datetime = models.DateTimeField(auto_now_add=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.customer.__str__() + "'s order " + str(self.ordered_datetime)
 
 
-class OrderLine(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, editable=False)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, editable=False)
-    quantity = models.IntegerField(editable=False)
-
-    def __str__(self):
-        return self.product.__str__() + " " + str(self.quantity)
-
-
 class Delivery(models.Model):
-    worker = models.ForeignKey(Worker, on_delete=models.CASCADE, editable=False)
-    stock = models.ForeignKey(Stock, on_delete=models, editable=False)
-    delivered_datetime = models.DateTimeField(auto_now_add=True, editable=False)
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    delivered_datetime = models.DateTimeField(auto_now_add=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.worker.__str__() + "'s delivery " + str(self.delivered_datetime)
-
-class DeliveryLine(models.Model):
-    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE, editable=False)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, editable=False)
-    quantity = models.IntegerField(editable=False)
-
-    def __str__(self):
-        return self.product.__str__() + " " + str(self.quantity)
