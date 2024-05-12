5ae55ec7eeeef74f574cfbab2cea99cb8a63337d
yerassylrymkul <rymkulerasyl@gmail.com>
2024-05-12 23:47:34 +0500

    final-3

--- final/finalProject/api/urls.py
+++ final/finalProject/api/urls.py
@@ -1,46 +1,21 @@
-from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
-from .views import (
-    LocationAPIDetailView,
-    LocationAPIView,
-    TourAPIView,
-    TourAPIDetailView,
-    ReviewAPIView,
-    ReviewAPIDetailView,
-    ReviewCreateAPIView,
-    RequestAPICreate,
-    RegistrationAPICreate,
-    RatingAPIView,
-    RatingAPIDetailView,
-    RatingCreateAPIView
+from rest_framework.routers import DefaultRouter
+from .views import LocationViewSet, TourViewSet, ReviewViewSet, RequestViewSet, RatingViewSet, tour_list
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-)
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+router = DefaultRouter()
+router.register(r'locations', LocationViewSet)
+router.register(r'tours', TourViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'requests', RequestViewSet)
+router.register(r'ratings', RatingViewSet)
 
 urlpatterns = [
-    path('locations/', LocationAPIView.as_view()),
-    path('locations/<int:pk>/', LocationAPIDetailView.as_view()),
-    path('locations/<int:location_id>/tours/', TourAPIView.as_view()),
-    path('locations/<int:location_id>/tours/<int:pk>', TourAPIDetailView.as_view()),
-
-    path('reviews/', ReviewAPIView.as_view()),
-    path('reviews/create/', ReviewCreateAPIView.as_view()),
-    path('reviews/<int:pk>/', ReviewAPIDetailView.as_view()),
-
-    path('ratings/', RatingAPIView.as_view()),
-    path('ratings/create/', RatingCreateAPIView.as_view()),
-    path('ratings/<int:pk>/', RatingAPIDetailView.as_view()),
-
+    path('tours/template', tour_list),
+    path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-
-    path('request/', RequestAPICreate.as_view()),
-    path('registration/', RegistrationAPICreate.as_view())
-
 ]
--- final/finalProject/api/views.py
+++ final/finalProject/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Location, Tour, Review, Request, Rating
 from .permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 from .serializers import LocationSerializer, TourSerializer, ReviewSerializer, RequestSerializer, UserSerializer, \
@@ -10,81 +12,70 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
-# list of Locations
-class LocationAPIView(generics.ListAPIView):
+def tour_list(request):
+    tours = Tour.objects.all()
+
+    name_filter = request.GET.get('name')
+    if name_filter:
+        tours = tours.filter(name__icontains=name_filter)
+
+    price_filter = request.GET.get('price')
+    if price_filter:
+        try:
+            price_filter = float(price_filter)
+            tours = tours.filter(price__lte=price_filter)
+        except ValueError:
+            pass
+
+    return render(request, 'tour_list.html', {'tours': tours})
+
+
+class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
     permission_classes = (IsOwnerOrReadOnly,)
 
 
-# Location detail
-class LocationAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# list of tours
-class TourAPIView(generics.ListAPIView):
+class TourViewSet(viewsets.ModelViewSet):
     queryset = Tour.objects.all()
     serializer_class = TourSerializer
     permission_classes = (IsOwnerOrReadOnly,)
 
+    @action(detail=True, methods=['get'])
+    def increase_price(self, request, pk=None):
+        tour = self.get_object()
+        # Increase the price of the tour by 10%
+        tour.price *= 1.1
+        tour.save()
+        return Response({'detail': 'Price increased by 10%'})
 
-# tour detail
-class TourAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Tour.objects.all()
-    serializer_class = TourSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    @action(detail=False, methods=['get'])
+    def cheap_tours(self, request):
+        # Retrieve tours with prices less than $100
+        cheap_tours = self.queryset.filter(price__lt=100)
+        serializer = self.get_serializer(cheap_tours, many=True)
+        return Response(serializer.data)
 
 
-# list of review
-class ReviewAPIView(generics.ListAPIView):
+class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = (IsOwnerOrReadOnly,)
 
 
-# create review
-class ReviewCreateAPIView(generics.CreateAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
-
-
-# review detail
-class ReviewAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
-
-
-class RequestAPICreate(generics.CreateAPIView):
+class RequestViewSet(viewsets.ModelViewSet):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
-class RegistrationAPICreate(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
-
-
-class RatingAPIView(generics.ListAPIView):
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
-
-
-class RatingCreateAPIView(generics.CreateAPIView):
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
-
-
-# review detail
-class RatingAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     permission_classes = (IsOwnerOrReadOnly,)
