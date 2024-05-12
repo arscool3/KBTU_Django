e802e60b878577eb3dfe7cccbee6945314af3e97
Tolganai Tleubaeva <120589023+Smonne@users.noreply.github.com>
2024-05-13 00:38:17 +0500

    saving the version without relationships 0038

--- final project/entity.py
+++ final project/entity.py
@@ -1,6 +1,28 @@
 from pydantic import BaseModel
 
 class User(BaseModel):
+    id:int
     username: str
+    class Config:
+       from_attributes = True
+class Recipe(BaseModel):
+    id:int
+    title: str
+    description: str
+    class Config:
+       from_attributes = True
+
+class Article(BaseModel):
+    id: int
+    
+    title: str
+    description: str
+    class Config:
+       from_attributes = True
+
+class Ingredient(BaseModel):
+    id: int
+    title: str
+    description: str
     class Config:
        from_attributes = True
