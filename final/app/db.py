69cd0de00856ad53dac0a573c84943b39464d94f
Baqbergen <onalbekovbakbergen7@gmail.com>
2024-05-12 22:41:00 +0500

    final

--- final/app/db.py
+++ final/app/db.py
@@ -11,5 +11,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
--- final/app/main.py
+++ final/app/main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
 
+from db import engine
+import models
+import users
+import comments
+import posts
+
 api = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+api.include_router(users.router)
+api.include_router(posts.router)
+api.include_router(comments.router)
