bc7cbfc10215b05203f802ff8586f5f32968252e
tenten <98640569+Hashira10@users.noreply.github.com>
2024-05-12 22:40:21 +0500

    Add files via upload

--- final/journey_journal_back/settings.py
+++ final/journey_journal_back/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'drf_spectacular',
     
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
