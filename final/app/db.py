6031444d6936e6a4b07b1d4fad8873919ae67701
enurach <nurali.cherniyazov03@gmail.com>
2024-05-18 12:45:41 +0500

    db

--- final/app/db.py
+++ final/app/db.py
@@ -9,3 +9,10 @@ DATABASE_URL = Settings.DATABASE_URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
