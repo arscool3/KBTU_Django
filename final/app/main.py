d63d66d56a044b9227fb0cdc312dd3b5f5b90972
enurach <nurali.cherniyazov03@gmail.com>
2024-05-21 22:32:32 +0500

    Tag and Field urls/controlers

---
+++ final/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import Depends, FastAPI
+from routes import user_routes, paper_routers, tag_routes, field_routes
+import models
+from db import engine
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(user_routes.router)
+app.include_router(paper_routers.router)
+app.include_router(tag_routes.router)
+app.include_router(field_routes.router)
+
