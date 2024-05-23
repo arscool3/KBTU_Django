fd39d7d34267515c889d8a75502ce2d584fe1de0
togzhanzhakhani <t7zhan@gmail.com>
2024-05-23 14:19:39 +0600

    dramatiq update

--- endterm+final/MIS/MediConnectBackend/doctors/dramatiq_settings.py
+++ endterm+final/MIS/MediConnectBackend/doctors/dramatiq_settings.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediConnectBackend.settings")
 django.setup()
 
 from django_dramatiq import get_broker
