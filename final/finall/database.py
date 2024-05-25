dc8726eeb624a85288d75fac53b7e6c193a5739f
Ilshat07 <98652504+Ilshat07@users.noreply.github.com>
2024-05-25 21:04:05 +0500

    Delete final directory

--- final/finall/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from databases import Database
-
-DATABASE_URL = "postgresql://username:password@localhost/my_fastapi_db"
-
-engine = create_engine(DATABASE_URL)
-database = Database(DATABASE_URL)
--- final/finall/tasks/email.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-from fastapi import BackgroundTasks
-from . import celery_app
-
-@celery_app.task
-def send_email_notification(email: str, message: str):
-    # Logic to send email notification
-    pass
