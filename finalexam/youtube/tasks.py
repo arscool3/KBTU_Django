b39926157107d0af9f94c04da3d8201f8d25ee58
21B031174 <98838429+21B031174@users.noreply.github.com>
2024-05-25 19:53:50 +0500

    finalExam

--- finalexam/youtube/tasks.py
+++ finalexam/youtube/tasks.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 @shared_task
 def send_email_task(subject, message, recipient_list):
     try:
-        send_mail(subject, message, 'nursturugeldiev@gmail.com', recipient_list)
+        send_mail(subject, message, 'elshan.arsen.q@gmail.com', recipient_list)
         return "Successfully sent email"
     except Exception as e:
         return str(e)
