d163a120f653d00add7b47ec1e084e2f057fd91a
pp221B040390 <manaskhairullin.gg@mail.ru>
2024-05-25 21:30:36 +0500

    celery

--- midtermdj/manga/tasks.py
+++ midtermdj/manga/tasks.py
@@ -3,10 +3,10 @@ from .models import Manga
 
 @shared_task
 def update_manga_ratings():
-    for manga in Manga.objects.all():
-        reviews = manga.reviews.all()
-        if reviews.exists():
-            avg_rating = sum(review.rating for review in reviews) / reviews.count()
-            manga.average_rating = avg_rating
-            manga.save()
+for manga in Manga.objects.all():
+    reviews = manga.reviews.all()
+    if reviews.exists():
+        avg_rating = sum(review.rating for review in reviews) / reviews.count()
+        manga.average_rating = avg_rating
+        manga.save()
 
--- midtermdj/midtermdj/settings.py
+++ midtermdj/midtermdj/settings.py
@@ -128,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
+
